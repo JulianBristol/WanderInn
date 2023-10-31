9b3cfa6c9792abf0c8accec3de425d91c3cc2762
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-10-31 15:48:31 -0400

    Add Tahiti color scheme and search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'tahiti': {
+        100: '#e49f9b',
+        200: '#e38984',
+        300: '#e37872',
+        400: '#e36861',
+        500: '#e2544b',
+        600: '#d44137',
+        700: '#cb3127',
+        800: '#b51d13',
+        900: '#ac1108',
+      },
+    },
+    },
   },
   plugins: [],
 }
