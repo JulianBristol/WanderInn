1c0a174cd7d7f043156fec118af6518912c4ab84
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-10-31 15:29:18 -0400

    Add Favicon image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/navbar/Navbar'
+import Favicon from '../public/images/WanderInn_logo_sm.png';
 
 const font = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'WanderInn',
   description: "Discover unique stays, from cozy cabins to urban lofts, and everything in between. With WanderInn, you'll find the ideal place to call home while you explore new destinations.",
+  authors: [{name: "Julian Bristol", url: "https://julianbristol.netlify.app" }],
+  icons: [{ rel: 'icon', url: Favicon.src }],
 }
 
 export default function RootLayout({
