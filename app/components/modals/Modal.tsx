25df32b47c6a757f86206c0ed738325ea03a341e
Julian Bristol <Bristoljulian.r@gmail.com>
2023-11-03 14:33:35 -0400

    Create modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { FC, useCallback, useEffect, useState } from 'react'
+import { IoMdClose } from 'react-icons/io'
 
 interface ModalProps{
     isOpen?: boolean;
@@ -34,14 +35,123 @@ const Modal: FC<ModalProps> = ({
         setShowModal(isOpen);
     }, [isOpen])
 
-    const handleOpen = useCallback(() => {
+    const handleClose = useCallback(() => {
+      if (disabled){
+        return;
+      }
 
-    }, []);
+      setShowModal(false);
+      setTimeout(() => {
+        onClose();
+      }, 300)
+    }, [disabled, onClose]);
+
+    const handleSubmit = useCallback(() => {
+      if (disabled){
+        return;
+      }
+
+      onSubmit();
+    }, [disabled, onSubmit]);
+
+    const handleSecondaryAction = useCallback(() => {
+      if (disabled || !secondaryAction) {
+        return;
+      }
+
+      secondaryAction();
+
+    }, [disabled, secondaryAction])
+
+    if (!isOpen){
+      return null;
+    }
 
   return (
-    <div>
-      modal
+    <>
+    <div
+    className='
+      justify-center
+      items-center
+      flex
+      overflow-x-hidden
+      overflow-y-auto
+      fixed
+      inset-0
+      z-50
+      bg-neutral-800/70
+      outline-none
+      focus:outline-none
+    '
+    >
+      <div
+      className='
+      relative
+      w-full
+      md:w-4/6
+      lg:w-3/6
+      xl:w-2/5
+      my-6
+      mx-auto
+      h-full
+      lg:h-auto
+      md:h-auto
+      '
+      >
+        {/* Content */}
+        <div
+        className={`
+        translate
+        duration-300
+        h-full
+        ${showModal ? "translate-y-0" : "translate-y-full"}
+        ${showModal ? "opacity-100" : "opacity-0"}
+        `}
+        >
+          <div className='
+          translate
+          h-full
+          lg:h-auto
+          md:h-auto
+          border-0
+          rounded-lg
+          shadow-lg
+          relative
+          flex
+          flex-col
+          w-full
+          bg-white
+          outline-none
+          focus:outline-none
+          '>
+            {/* Header */}
+            <div className='
+            flex
+            items-center
+            p-6
+            rounded-t
+            justify-center
+            relative
+            border-b-[1px]
+            '>
+              <button className='
+              hover:opacity-70
+              p-1
+              border-0
+              absolute
+              left-9
+              transition
+              '
+              onClick={handleClose}
+              >
+                <IoMdClose size={18} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
 
