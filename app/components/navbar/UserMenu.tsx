9c5e97b282f9b01d94c4258235378de8ef00450d
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-11-09 14:34:15 -0500

    Add menu buttons and Log out functionality

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,33 +1,39 @@
 "use client"
 
-import React, { useCallback, useState } from 'react'
+import React, { FC, useCallback, useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
+import { User } from '@prisma/client';
+import { signOut } from 'next-auth/react'
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser?: User | null;
+}
+
+const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
-  },[])
+  }, [])
 
   return (
     <div className='relative'>
       <div className='flex flex-row items center gap-3'>
         <div
-        className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer' 
-        onClick={() => {}}
+          className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
+          onClick={() => { }}
         >
           WanderInn Your Home
         </div>
         <div
-        onClick={toggleOpen}
-        className='
+          onClick={toggleOpen}
+          className='
         p-4
         md:py-1
         md:px-2
@@ -52,16 +58,47 @@ const UserMenu = () => {
       {isOpen && (
         <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
           <div className='flex flex-col cursor-pointer'>
+            {currentUser ? 
+            (
             <>
-            <MenuItem 
-            onClick={loginModal.onOpen}
-            label="Log In"
+            <MenuItem
+              onClick={() => {}}
+              label="My Trips"
             />
-            <MenuItem 
-            onClick={registerModal.onOpen}
-            label="Sign Up"
+            <MenuItem
+              onClick={() => {}}
+              label="My Favorites"
+            />
+            <MenuItem
+              onClick={() => {}}
+              label="My Reservations"
+            />
+            <MenuItem
+              onClick={() => {}}
+              label="My Properties"
+            />
+            <MenuItem
+              onClick={() => {}}
+              label="WanderInn My Home"
+            />
+            <hr />
+            <MenuItem
+              onClick={() => signOut()}
+              label="Log Out"
             />
             </>
+            ) : (
+              <>
+                <MenuItem
+                  onClick={loginModal.onOpen}
+                  label="Log In"
+                />
+                <MenuItem
+                  onClick={registerModal.onOpen}
+                  label="Sign Up"
+                />
+              </>)
+            }
           </div>
         </div>
       )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ClientOnly from './components/ClientOnly';
 import LoginModal from './components/modals/LoginModal';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
+import getCurrentUser from './actions/getCurrentUser';
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -17,11 +18,12 @@ export const metadata: Metadata = {
   icons: [{ rel: 'icon', url: Favicon.src }],
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
@@ -29,7 +31,7 @@ export default function RootLayout({
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser}/>
         </ClientOnly>
         {children}
         </body>
